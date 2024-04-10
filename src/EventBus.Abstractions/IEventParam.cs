67a69bf1b263c6369c05e779852ff06681a2231b
XmmShp <79584305+XmmShp@users.noreply.github.com>
2024-04-10 21:04:31 +0800

    feat: KeyedParam
    #6

--- src/EventBus.Abstractions/IEventParam.cs
+++ src/EventBus.Abstractions/IEventParam.cs
@@ -9,4 +9,14 @@ public interface IEventParam
     T Get<T>(int index) where T : class;
     bool TryGet<T>(int index, out T value) where T : class;
     void Push(object value);
+    bool Remove(int index);
+
+    object Get(string name) => Get<object>(name);
+    T Get<T>(string name) where T : class;
+    bool TryGet<T>(string name, out T value) where T : class;
+    void Add(string name, object value);
+    bool TryAdd(string name, object value);
+    void SetName(int index, string name);
+    bool TrySetName(int index,string name);
+    bool Remove(string name);
 }
--- src/EventBus/EventParam.cs
+++ src/EventBus/EventParam.cs
@@ -12,6 +12,13 @@ public class EventParam : IEventParam
     {
         _values = new List<object?>(values);
     }
+
+    public EventParam(IDictionary<string, object?> values)
+    {
+        _values = new List<object?>(values.Values);
+        _map = values.Keys.Select((key, index) => (key, index)).ToDictionary(pair => pair.key, pair => pair.index);
+    }
+
     public EventStatus Status { get; set; }
 
     public T Get<T>(int index) where T : class
@@ -33,9 +40,69 @@ public class EventParam : IEventParam
         }
     }
 
+    public bool Remove(int index)
+    {
+        if (index < 0 || index >= _values.Count)
+        {
+            return false;
+        }
+        _values.RemoveAt(index);
+        return true;
+    }
+
+    public T Get<T>(string name) where T : class => Get<T>(_map[name]);
+
+    public bool TryGet<T>(string name, out T value) where T : class
+    {
+        if (_map.TryGetValue(name, out var index))
+        {
+            return TryGet(index, out value);
+        }
+        value = default!;
+        return false;
+    }
     public void Push(object value)
     {
         _values.Add(value);
     }
+
+    public void Add(string name, object value)
+    {
+        _map[name] = _values.Count;
+        Push(value);
+    }
+    public bool TryAdd(string name, object value)
+    {
+        if (_map.ContainsKey(name))
+        {
+            return false;
+        }
+        Add(name, value);
+        return true;
+    }
+
+    public void SetName(int index, string name)
+    {
+        _map[name] = index;
+    }
+
+    public bool TrySetName(int index, string name)
+    {
+        if (index < 0 || index >= _values.Count || _map.ContainsKey(name))
+        {
+            return false;
+        }
+        SetName(index, name);
+        return true;
+    }
+
+    public bool Remove(string name)
+    {
+        if (!_map.Remove(name, out var index)) return false;
+        _values.RemoveAt(index);
+        return true;
+    }
+
     private readonly List<object?> _values;
+    private readonly Dictionary<string, int> _map = [];
 }
