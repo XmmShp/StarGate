9c7d08fb5f1c7e6d1ca325b7f3197eb812ab994c
XmmShp <79584305+XmmShp@users.noreply.github.com>
2024-09-02 05:20:05 +0800

    feat: StarBelt

--- src/StarGate/Abstractions/IStarBelt.cs
+++ src/StarGate/Abstractions/IStarBelt.cs
@@ -1,24 +1,17 @@
 ﻿using StarGate.Adapter;
-using StarGate.Fundamental;
 using System.Collections;
-using System.Threading.Tasks;
 
 namespace StarGate.Abstractions
 {
     public interface IStarBelt<T>
     {
-        StarResult<T> Invoke(IStarParam param);
+        T Invoke(IStarParam param);
         void RegisterHandler(Functor<T> handler);
 
         #region derived functions
-        StarResult<T> Invoke(IEnumerable param) => Invoke(new StarParam(param));
-        StarResult<T> Invoke(IDictionary param) => Invoke(new StarParam(param));
-        StarResult<T> Invoke(params object?[] param) => Invoke(new StarParam(param));
-
-        async Task<StarResult<T>> InvokeAsync(IStarParam param) => await Task.Run(() => Invoke(param));
-        Task<StarResult<T>> InvokeAsync(IEnumerable param) => InvokeAsync(new StarParam(param));
-        Task<StarResult<T>> InvokeAsync(IDictionary param) => InvokeAsync(new StarParam(param));
-        Task<StarResult<T>> InvokeAsync(params object?[] param) => InvokeAsync(new StarParam(param));
+        T Invoke(IEnumerable param) => Invoke(new StarParam(param));
+        T Invoke(IDictionary param) => Invoke(new StarParam(param));
+        T Invoke(params object?[] param) => Invoke(new StarParam(param));
         #endregion
     }
 }
--- src/StarGate/StarBelt.cs
+++ src/StarGate/StarBelt.cs
@@ -1,7 +1,6 @@
 ﻿using StarGate.Abstractions;
 using StarGate.Adapter;
 using StarGate.Enums;
-using StarGate.Fundamental;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,27 +9,22 @@ namespace StarGate
     public class StarBelt<T> : IStarBelt<T>
     {
         public StarBelt() { _handlers.Add(new NullStarHandler<T>()); }
-        public StarResult<T> Invoke(IStarParam param)
+        public T Invoke(IStarParam param)
         {
-            T? result = default;
-            if (!param.Status.IsInterrupt())
+            T result = default!;
+            foreach (var handler in _handlers.TakeWhile(_ => !param.Status.IsInterrupted()))
             {
-                foreach (var handler in _handlers.TakeWhile(_ => !param.Status.IsInterrupt()))
-                {
-                    var res = handler.Invoke(param);
-                    param.Status |= res;
-                    if (param.Status.IsSolve())
-                    {
-                        result = res.Return;
-                    }
-                }
+                var res = handler.Invoke(param);
+                if (!param.Status.IsSolved()) continue;
+                result = res;
+                break;
             }
-            return (result, param.Status);
+            return result;
         }
 
         public void RegisterHandler(Functor<T> handler)
         {
-            _handlers.Add(new StarHandler<T>(handler));
+            _handlers.Insert(_handlers.Count - 1, new StarHandler<T>(handler));
         }
 
         private readonly List<StarHandler<T>> _handlers = new();
